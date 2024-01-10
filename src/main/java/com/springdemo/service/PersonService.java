75645300502b545147bfb2aa0045b3cab953d700
vihandesilva <vihandesilva@gmail.com>
2024-01-10 14:09:39 +0530

    updating postgresql

--- src/main/java/com/springdemo/service/PersonService.java
+++ src/main/java/com/springdemo/service/PersonService.java
@@ -7,18 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springdemo.entities.Person;
-import com.springdemo.repositories.PersonRepository;
 
 @Service
 public class PersonService {
-	private List<Person> personList;
+	
+	private List<Person> personList;	
 	
 	@Autowired
-	private PersonRepository personRepository;
-	
-	@Autowired
-	public PersonService(PersonRepository repo) {
-		this.personRepository = repo;
+	public PersonService() {
 		personList = new ArrayList<>();
 		personList.add(new Person("2000X", "Carl", "Johnson", 1800666));
 	}
